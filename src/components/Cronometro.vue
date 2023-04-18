0d66d640b12da16b3fde3c021d5c5b564184d0a1
Ygor Alberto <ygormorais@hotmail.com>
2023-04-18 18:57:40 -0300

    Refatorando formulário do timer

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <strong>
+            {{ tempoDecorrido }}
+        </strong>
+    </section>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+    name: 'Cronometro',
+    props: {
+        tempoEmSegundo: {
+            type: Number,
+            default: 0
+        }
+    },
+    computed: {
+        tempoDecorrido(): string {
+            return new Date(this.tempoEmSegundo * 1000).toISOString().substr(11, 8)
+        }
+    },
+})
+</script>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulario para criação de uma tarefa">
+                <input type="text" class="input" placeholder="Qual tarefe você deseja iniciar">
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+
+                    <Cronometro :tempoEmSegundo ="tempoEmSegundo" />
+                    <button class="button" @click="iniciar">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button" @click="finalizar">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import Cronometro from './Cronometro.vue'
+
+export default defineComponent({
+    name: "Formulário",
+    components: {
+        Cronometro
+
+    },
+    data() {
+        return {
+            tempoEmSegundo: 0,
+            cronometro: 0
+        }
+    },
+
+    methods: {
+        iniciar() {
+            // começar a contagem
+            //1s = a 1000ms
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundo += 1
+            }, 1000)
+
+
+        },
+        finalizar() {
+            clearInterval(this.cronometro)
+
+        }
+    }
+});
+</script>
